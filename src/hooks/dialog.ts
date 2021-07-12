7349d415ed3e62b6cba6ace4a00cbff86dc26ae9
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-12 15:34:25 -0300

    Segundo deploy

--- src/hooks/dialog.ts
+++ src/hooks/dialog.ts
@@ -27,6 +27,35 @@ export default class DialogHook {
             }
           }*/
             //return response.json(serializedNoticias)
+            let jsonDialog = request.body.fulfillmentMessages.push({
+                
+                    "attachment":{
+                      "type":"template",
+                      "payload":{
+                        "template_type":"generic",
+                        "elements":[
+                           {
+                            "title":"Welcome!",
+                            "image_url":"https://petersfancybrownhats.com/company_image.png",
+                            "subtitle":"We have the right hat for everyone.",
+                            "buttons":[
+                              {
+                                "type":"web_url",
+                                "url":"https://petersfancybrownhats.com",
+                                "title":"View Website"
+                              },{
+                                "type":"postback",
+                                "title":"Start Chatting",
+                                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                              }              
+                            ]      
+                          }
+                        ]
+                      }
+                    }
+                
+            })
+            /*
             let responseJson =
                 {
                     "fulfillmentMessages": [
@@ -60,8 +89,8 @@ export default class DialogHook {
                         }
                     ],
                     //"source": "webhook"
-                }
-            response.json(responseJson)
+                }*/
+            response.json(jsonDialog)
         }
 
 

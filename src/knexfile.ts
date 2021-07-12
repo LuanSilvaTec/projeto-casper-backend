ee5e1b889050978f7a45a2facc8a0ab78bf4e47a
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-11 23:15:21 -0300

    Primeiro deploy

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -28,17 +28,9 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection:process.env.DATABASE_URL,
     migrations: {
-      tableName: "knex_migrations"
+      directory:__dirname +'/database/migrations',
     }
   }
 

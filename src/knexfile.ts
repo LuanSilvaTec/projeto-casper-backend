0042494ded1d089fdcba970da2b1d15d9661c678
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-11 23:25:20 -0300

    Deploy backend

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -9,23 +9,6 @@ module.exports = {
     debug:true,
     useNullAsDefault:true
   },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
     client: "pg",
     connection:process.env.DATABASE_URL,

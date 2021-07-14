a6ec6dc00c2bff2a97f6467e49f0e494fff67a60
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-14 11:03:08 -0300

    Testes de integração com o facebook

--- src/hooks/dialog.ts
+++ src/hooks/dialog.ts
@@ -22,109 +22,126 @@ export default class DialogHook {
     console.log("Teste de integração para busca de noticias sobre " + intent);
     const noticias = await knex('noticias').select('*').where('tema', intent).limit(10);
     //console.log(noticias);
-    console.log("Tamanho do array: "+noticias.length);
-    const serializedNoticias = noticias.map(noticia => {
-      return {
-        image_url: noticia.url_imagem,
-        title: noticia.titulo,
-        subtitle: noticia.descricao,
-        url_noticia: noticia.url_noticia
-      }
-    })
-    //console.log(serializedNoticias)
-    /*Sem parâmetro de payload */
-    //console.log(request.body.queryResult.fulfillmentMessages.push(responseJson))
-    //console.log(request.body.queryResult.fulfillmentMessages)
-    //console.log(request.body)
-    //console.log(request.body.queryResult)
-    let responseJson = {
-      "fulfillmentMessages": [
-        {
-          "payload": {
-            "facebook": {
-              "attachment": {
-                "type": "template",
-                "payload": {
-                  "template_type": "generic",
-                  "elements": [
+    const statusRetorno = noticias.length
 
-                  ]
+    if (statusRetorno != 0) {
+      const serializedNoticias = noticias.map(noticia => {
+        return {
+          image_url: noticia.url_imagem,
+          title: noticia.titulo,
+          subtitle: noticia.descricao,
+          url_noticia: noticia.url_noticia
+        }
+      })
+      //console.log(serializedNoticias)
+      /*Sem parâmetro de payload */
+      //console.log(request.body.queryResult.fulfillmentMessages.push(responseJson))
+      //console.log(request.body.queryResult.fulfillmentMessages)
+      //console.log(request.body)
+      //console.log(request.body.queryResult)
+      let responseJson = {
+        "fulfillmentMessages": [
+          {
+            "payload": {
+              "facebook": {
+                "attachment": {
+                  "type": "template",
+                  "payload": {
+                    "template_type": "generic",
+                    "elements": [
+
+                    ]
+                  }
                 }
               }
-            }
-          },
-          "platform": "FACEBOOK"
-        }
-      ]
-    }
-
-    const elements = []
-    for(var i=0; i<serializedNoticias.length; i++){
-      let obj = {
-        "title": serializedNoticias[i].title,
-        "subtitle": serializedNoticias[i].subtitle,
-        "image_url": serializedNoticias[i].image_url,
-        "buttons": [
-          {
-            "title": "View",
-            "type": "web_url",
-            "url": serializedNoticias[i].url_noticia
-          },
+            },
+            "platform": "FACEBOOK"
+          }
         ]
       }
-      console.log(obj)
-      responseJson.fulfillmentMessages[0].payload.facebook.attachment.payload.elements.push(obj)
-    }
 
-    console.log(responseJson.fulfillmentMessages[0].payload.facebook.attachment)
-    
-    //responseJson.fulfillmentMessages[0].payload.facebook.attachment.payload.elements.push(elements)
+      const elements = []
+      for (var i = 0; i < serializedNoticias.length; i++) {
+        let obj = {
+          "title": serializedNoticias[i].title,
+          "subtitle": serializedNoticias[i].subtitle,
+          "image_url": serializedNoticias[i].image_url,
+          "buttons": [
+            {
+              "title": "View",
+              "type": "web_url",
+              "url": serializedNoticias[i].url_noticia
+            },
+          ]
+        }
+        console.log(obj)
+        responseJson.fulfillmentMessages[0].payload.facebook.attachment.payload.elements.push(obj)
+      }
 
-    /*
-    let responseJson={
-      "fulfillmentMessages": [
-        {
-          "payload": {
-            "facebook": {
-              "attachment": {
-                "type": "template",
-                "payload": {
-                  "template_type": "generic",
-                  "elements": [
-                    {
-                      "title": "Welcome LUAN SILVA!",
-                      "subtitle": "We have the right hat for everyone.",
-                      "image_url": "https://petersfancybrownhats.com/company_image.png",
-                      "buttons": [
-                        {
-                          "title": "View Website",
-                          "type": "web_url",
-                          "url": "https://petersfancybrownhats.com"
-                        },
-                      ]
-                    },
-                    {
-                      "buttons": [
-                        {
-                          "url": "https://petersfancybrownhats.com",
-                          "title": "View Website",
-                          "type": "web_url"
-                        }
-                      ],
-                      "image_url": "https://petersfancybrownhats.com/company_image.png",
-                      "title": "Welcome SILVA!",
-                      "subtitle": "We have the right hat for everyone."
-                    }
-                  ]
+      console.log(responseJson.fulfillmentMessages[0].payload.facebook.attachment)
+
+      //responseJson.fulfillmentMessages[0].payload.facebook.attachment.payload.elements.push(elements)
+
+      /*
+      let responseJson={
+        "fulfillmentMessages": [
+          {
+            "payload": {
+              "facebook": {
+                "attachment": {
+                  "type": "template",
+                  "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                      {
+                        "title": "Welcome LUAN SILVA!",
+                        "subtitle": "We have the right hat for everyone.",
+                        "image_url": "https://petersfancybrownhats.com/company_image.png",
+                        "buttons": [
+                          {
+                            "title": "View Website",
+                            "type": "web_url",
+                            "url": "https://petersfancybrownhats.com"
+                          },
+                        ]
+                      },
+                      {
+                        "buttons": [
+                          {
+                            "url": "https://petersfancybrownhats.com",
+                            "title": "View Website",
+                            "type": "web_url"
+                          }
+                        ],
+                        "image_url": "https://petersfancybrownhats.com/company_image.png",
+                        "title": "Welcome SILVA!",
+                        "subtitle": "We have the right hat for everyone."
+                      }
+                    ]
+                  }
                 }
               }
+            },
+            "platform": "FACEBOOK"
+          }
+        ]
+      }*/
+      response.json(responseJson)
+
+    }
+    else {
+      response.json({
+        "fulfillmentMessages": [
+          {
+            "text": {
+              "text": [
+                "Não há notícias cadastradas sobre " + intent
+              ]
             }
-          },
-          "platform": "FACEBOOK"
-        }
-      ]
-    }*/
-    response.json(responseJson)
+          }
+        ]
+      })
+    }
 
 
 

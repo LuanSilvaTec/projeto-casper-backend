aac33de6520e2d52289f3ca18547e232f1b12f82
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-14 08:36:43 -0300

    Testes de integração com o facebook

--- src/hooks/dialog.ts
+++ src/hooks/dialog.ts
@@ -79,59 +79,33 @@ export default class DialogHook {
 
       response.json({
         "fulfillmentMessages": [
+          {
+            "text": {
+              "text": [
+                "Você escolheu Famosos"
+              ]
+            },
+            "platform": "FACEBOOK"
+          },
+          {
+            "card": {
+              "title": "Bem vindo ao nosso processo seletivo",
+              "subtitle": "Bem vindo ao nosso processo seletivo",
+              "imageUri": "https://firebasestorage.googleapis.com/v0/b/universidade-3d7f8.appspot.com/o/processo%2Fprocesso-seletivo.png?alt=media&token=d5a89cec-1c07-4dad-9b44-7aaf098128bd",
+              "buttons": [
                 {
-                  "card": {
-                    "title": "Processo Seletivo Luan silva",
-                    "subtitle": "Bem vindo ao nosso processo seletivo",
-                    "imageUri": "https://firebasestorage.googleapis.com/v0/b/universidade-3d7f8.appspot.com/o/processo%2Fprocesso-seletivo.png?alt=media&token=d5a89cec-1c07-4dad-9b44-7aaf098128bd"
-                  }
+                  "text": "Abrir"
                 },
                 {
-                  "text" :{
-                     "text": [
-                        "Temos os melhores cursos nas areas de Humanas, exatas e Biologicas"
-                    ]
-                  }
-                },
-                {
-                  "image":{
-                    
-                      "imageUri": "https://firebasestorage.googleapis.com/v0/b/universidade-3d7f8.appspot.com/o/processo%2Falunos_campus.jpg?alt=media&token=af408613-e87c-4f2c-982e-dbc1b5d29748",
-                      "accessibilityText": "Temos Campus em todas as areas da cidade"
-                    
-                  }
-                },
-                {
-                  "text" :{
-                     "text": [
-                        "Nossos Campus são os melhores do Brasil"
-                    ]
-                  }
-                },
-                {
-                  "image":{
-                    
-                      "imageUri": "https://firebasestorage.googleapis.com/v0/b/universidade-3d7f8.appspot.com/o/processo%2Flaboratorio.jpg?alt=media&token=83f17a55-5943-4592-a06f-64230f07afe2",
-                      "accessibilityText": "Laboratorios"
-                    
-                  }
-                },
-                {
-                  "text" :{
-                     "text": [
-                        "Temos os melhores laboratorios"
-                    ]
-                  }
-                },
-                {
-                  "text" :{
-                     "text": [
-                        "Voce quer participar do processo seletivo ?"
-                    ]
-                  }
+                  "text": "Abrir",
+                  "postback": "https://www.twitch.tv/luansilva_7"
                 }
               ]
-       });
+            },
+            "platform": "FACEBOOK"
+          }
+        ]
+      });
       //console.log(request.body.queryResult.fulfillmentMessages[1].payload.facebook.attachment.payload.elements)
     }
   }

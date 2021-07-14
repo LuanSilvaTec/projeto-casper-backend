5c4d681f5b5b73458a8fa763e661e23ee32deb8b
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-14 09:45:17 -0300

    Testes de integração com o facebook

--- src/hooks/dialog.ts
+++ src/hooks/dialog.ts
@@ -1,149 +1,127 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection'
 export default class DialogHook {
-  
+
   async dialog(request: Request, response: Response) {
     let intent
-    
+
     var intentName = request.body.queryResult.intent.displayName;
     if (intentName == "noticias.famosos") {
-      intent='Famosos'
+      intent = 'Famosos'
     }
     else if (intentName == "noticias.esportes") {
-      intent='Esportes'
+      intent = 'Esportes'
     }
     else if (intentName == "noticias.entretenimento") {
-      intent='Entretenimento'
+      intent = 'Entretenimento'
     }
     else if (intentName == "noticias.politica") {
-      intent='Política'
+      intent = 'Política'
     }
 
-      console.log("Teste de integração para busca de noticias sobre "+intent);
-      const noticias = await knex('noticias').select('*').where('tema', intent).limit(10);
-      //console.log(noticias);
-      const serializedNoticias = noticias.map(noticia => {
-        return {
-          image_url: noticia.url_imagem,
-          title: noticia.titulo,
-          subtitle: noticia.descricao,
-          url_noticia: noticia.url_noticia
-        }
-      })
-      
-      /*Sem parâmetro de payload */
-      //console.log(request.body.queryResult.fulfillmentMessages.push(responseJson))
-      //console.log(request.body.queryResult.fulfillmentMessages)
-      //console.log(request.body)
-      //console.log(request.body.queryResult)
-
-      let responseJson={
-        "fulfillmentMessages": [
+    console.log("Teste de integração para busca de noticias sobre " + intent);
+    const noticias = await knex('noticias').select('*').where('tema', intent).limit(10);
+    //console.log(noticias);
+    const serializedNoticias = noticias.map(noticia => {
+      return {
+        image_url: noticia.url_imagem,
+        title: noticia.titulo,
+        subtitle: noticia.descricao,
+        url_noticia: noticia.url_noticia
+      }
+    })
+    console.log(serializedNoticias)
+    /*Sem parâmetro de payload */
+    //console.log(request.body.queryResult.fulfillmentMessages.push(responseJson))
+    //console.log(request.body.queryResult.fulfillmentMessages)
+    //console.log(request.body)
+    //console.log(request.body.queryResult)
+    const elements = []
+    for(var i=0; i<serializedNoticias.length; i++){
+      let obj = {
+        "title": serializedNoticias[i].title,
+        "subtitle": serializedNoticias[i].subtitle,
+        "image_url": serializedNoticias[i].image_url,
+        "buttons": [
           {
-            "payload": {
-              "facebook": {
-                "attachment": {
-                  "type": "template",
-                  "payload": {
-                    "template_type": "generic",
-                    "elements": [
-                      {
-                        "title": "Welcome LUAN SILVA!",
-                        "subtitle": "We have the right hat for everyone.",
-                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                        "buttons": [
-                          {
-                            "title": "View Website",
-                            "type": "web_url",
-                            "url": "https://petersfancybrownhats.com"
-                          },
-                        ]
-                      },
-                      {
-                        "buttons": [
-                          {
-                            "url": "https://petersfancybrownhats.com",
-                            "title": "View Website",
-                            "type": "web_url"
-                          }
-                        ],
-                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                        "title": "Welcome SILVA!",
-                        "subtitle": "We have the right hat for everyone."
-                      }
-                    ]
-                  }
-                }
-              }
-            },
-            "platform": "FACEBOOK"
-          }
+            "title": "View",
+            "type": "web_url",
+            "url": serializedNoticias[i].url_noticia
+          },
         ]
       }
-      response.json(responseJson)
-      /*
-      response.json({
-        "fulfillmentMessages": [
-          {
-            "text": {
-              "text": [
-                "Você escolheu Famosos"
-              ]
-            },
-            "platform": "FACEBOOK"
-          },
-          {
-            "payload": {
-              "facebook": {
-                "attachment": {
-                  "type": "template",
-                  "payload": {
-                    "template_type": "generic",
-                    "elements": [
-                      {
-                        "title": "Welcome LUAN SILVA!",
-                        "subtitle": "We have the right hat for everyone.",
-                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                        "buttons": [
-                          {
-                            "title": "View Website",
-                            "type": "web_url",
-                            "url": "https://petersfancybrownhats.com"
-                          },
-                          {
-                            "type": "postback",
-                            "payload": "DEVELOPER_DEFINED_PAYLOAD",
-                            "title": "Start Chatting"
-                          }
-                        ]
-                      },
-                      {
-                        "buttons": [
-                          {
-                            "url": "https://petersfancybrownhats.com",
-                            "title": "View Website",
-                            "type": "web_url"
-                          },
-                          {
-                            "title": "Start Chatting",
-                            "type": "postback",
-                            "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                          }
-                        ],
-                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                        "title": "Welcome SILVA!",
-                        "subtitle": "We have the right hat for everyone."
-                      }
-                    ]
-                  }
+      elements.push(obj)
+    }
+
+    console.log(elements)
+    let responseJson = {
+      "fulfillmentMessages": [
+        {
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "generic",
+                  "elements": [
+
+                  ]
                 }
               }
-            },
-            "platform": "FACEBOOK"
-          }
-        ]
-      });*/
-    
+            }
+          },
+          "platform": "FACEBOOK"
+        }
+      ]
+    }
+
+
+    /*
+    let responseJson={
+      "fulfillmentMessages": [
+        {
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "generic",
+                  "elements": [
+                    {
+                      "title": "Welcome LUAN SILVA!",
+                      "subtitle": "We have the right hat for everyone.",
+                      "image_url": "https://petersfancybrownhats.com/company_image.png",
+                      "buttons": [
+                        {
+                          "title": "View Website",
+                          "type": "web_url",
+                          "url": "https://petersfancybrownhats.com"
+                        },
+                      ]
+                    },
+                    {
+                      "buttons": [
+                        {
+                          "url": "https://petersfancybrownhats.com",
+                          "title": "View Website",
+                          "type": "web_url"
+                        }
+                      ],
+                      "image_url": "https://petersfancybrownhats.com/company_image.png",
+                      "title": "Welcome SILVA!",
+                      "subtitle": "We have the right hat for everyone."
+                    }
+                  ]
+                }
+              }
+            }
+          },
+          "platform": "FACEBOOK"
+        }
+      ]
+    }*/
+    response.json(responseJson)
+
 
 
 

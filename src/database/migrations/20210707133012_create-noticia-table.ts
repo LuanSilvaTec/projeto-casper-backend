ee45a0a30df7c21840fee19c74c63687bd881489
LuanSilvaTec <tecnicoluansilva@hotmail.com>
2021-07-14 10:27:10 -0300

    Testes de integração com o facebook

--- src/database/migrations/20210707133012_create-noticia-table.ts
+++ src/database/migrations/20210707133012_create-noticia-table.ts
@@ -4,9 +4,9 @@ import  Knex  from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('noticias', table=>{
         table.increments('id').primary().unique();
-        table.string('url_imagem').notNullable();
+        table.string('url_imagem',1000).notNullable();
         table.string('titulo').notNullable();
-        table.string('descricao').notNullable();
+        table.string('descricao',1000).notNullable();
         table.string('tema').notNullable();
         table.string('url_noticia').notNullable();
         
--- src/hooks/dialog.ts
+++ src/hooks/dialog.ts
@@ -75,7 +75,7 @@ export default class DialogHook {
       responseJson.fulfillmentMessages[0].payload.facebook.attachment.payload.elements.push(obj)
     }
 
-    console.log(responseJson.fulfillmentMessages[0].payload)
+    console.log(responseJson.fulfillmentMessages[0].payload.facebook.attachment)
     
     //responseJson.fulfillmentMessages[0].payload.facebook.attachment.payload.elements.push(elements)
 
